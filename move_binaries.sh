72b12c7d7d7b5770d0c9bb17a6016a4ec3464f19
MZandtheRaspberryPi <mzandtheraspberrypi@gmail.com>
2026-05-29 15:38:17 +0100

    updating for jetson

--- move_binaries.sh
+++ move_binaries.sh
@@ -21,24 +21,30 @@ case $(uname -m) in
     i386)   architecture="386" ;;
     i686)   architecture="386" ;;
     x86_64) architecture="amd64" ;;
+    aarch64) architecture="arm64" ;;
     arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
 esac
 
+echo "architecture: $architecture"
+
 target_dir=""
 unziped_dir=""
-if [ $architecture = "amd64" ]; then
+if [ "$architecture" = "amd64" ]; then
   target_dir="CameraSDK-2.1.1-Linux.tar.gz"
   unzipped_dir="CameraSDK-20251104_115504-2.1.1.1-Linux"
-elif [ $architecture = "arm64" ] && [ -f "/sys/module/tegra_fuse/parameters/tegra_chip_id" ]; then
+elif [ "$architecture" = "arm64" ] && \
+  grep -q "Jetson" /sys/firmware/devicetree/base/model; then
   target_dir="CameraSDK-2.1.1-jetson-linux-9.3.0-2020.08-x86_64_aarch64_linux-gnu.tar.gz"
   unzipped_dir="CameraSDK-20251105_112855-2.1.1-jetson-linux-9.3.0-2020.08-x86_64_aarch64_linux-gnu"
-elif [ $architecture = "arm64" ]; then
+elif [ "$architecture" = "arm64" ]; then
   target_dir="CameraSDK-2.1.1-gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu.tar.gz"
   unzipped_dir="CameraSDK-20251105_140609-2.1.1-gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu"
 else
   echo -e "Can't find architecture"
   exit 1
 fi
+
+echo "target dir: $target_dir"
 
 tar -xzf ./Linux_CameraSDK-2.1.1_MediaSDK-3.1.1/$target_dir
 
